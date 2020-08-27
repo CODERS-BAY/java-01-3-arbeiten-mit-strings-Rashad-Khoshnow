09ec7dedb23e046879ab278f73d6a9ef8851be9b
Rashad <rashad.khoshnow@gmail.com>
2020-08-27 15:34:10 +0200

    nicht fertig

---
+++ src/aufgabe/einsPunktDrei.java
@@ -0,0 +1,14 @@
+package aufgabe;
+
+public class einsPunktDrei{
+
+    public static void main(String[] args) {
+
+        String helloWorld = " Hello World! ";
+        System.out.println(helloWorld);
+        System.out.println(helloWorld.toUpperCase());
+        System.out.println(helloWorld.toLowerCase());
+        String raplaceString = helloWorld.replace("'World!","Codersbay");
+        System.out.println();
+    }
+}
